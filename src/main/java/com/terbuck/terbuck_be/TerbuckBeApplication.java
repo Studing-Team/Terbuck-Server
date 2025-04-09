82c793c334c2b752891bcd7a3481608cd0363cfe
Han <hkjbrian@naver.com>
2025-04-09 22:27:56 +0900

    프로젝트 초기설정

---
+++ src/main/java/com/terbuck/terbuck_be/TerbuckBeApplication.java
@@ -0,0 +1,13 @@
+package com.terbuck.terbuck_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerbuckBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerbuckBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/terbuck/terbuck_be/TerbuckBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terbuck.terbuck_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerbuckBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
