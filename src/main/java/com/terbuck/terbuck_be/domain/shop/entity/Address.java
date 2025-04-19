e8cb3e226612100e48e8129c2898133b04ac8cf2
Han <hkjbrian@naver.com>
2025-04-19 17:28:05 +0900

    feat(Address, Location) : Address, Location 불변 객체 설정 수정

--- src/main/java/com/terbuck/terbuck_be/domain/shop/entity/Address.java
+++ src/main/java/com/terbuck/terbuck_be/domain/shop/entity/Address.java
@@ -1,9 +1,14 @@
 package com.terbuck.terbuck_be.domain.shop.entity;
 
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
-public abstract class Address {
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Address {
 
     private String region;
 
--- src/main/java/com/terbuck/terbuck_be/domain/shop/entity/Location.java
+++ src/main/java/com/terbuck/terbuck_be/domain/shop/entity/Location.java
@@ -1,9 +1,14 @@
 package com.terbuck.terbuck_be.domain.shop.entity;
 
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
-public abstract class Location {
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Location {
 
     private double latitude;
 
