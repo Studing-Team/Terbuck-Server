bdc42168f7b2f3016ca662580cd9f52e38d084eb
Han <hkjbrian@naver.com>
2025-04-22 17:44:49 +0900

    feat(auth) : 소셜로부터 받은 인가 코드 기반으로 accessToken 요청, accessToken 을 통한 사용자 정보 요청 기능 KakaoOAuthService 구현

---
+++ src/main/java/com/terbuck/terbuck_be/domain/auth/service/KakaoOAuthService.java
@@ -0,0 +1,83 @@
+package com.terbuck.terbuck_be.domain.auth.service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.terbuck.terbuck_be.common.enums.SocialType;
+import com.terbuck.terbuck_be.domain.auth.dto.UserInfo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+public class KakaoOAuthService {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Value("${kakao.client-id}")
+    private String clientId;
+
+    @Value("${kakao.redirect-uri}")
+    private String redirectUri;
+
+    @Value("${kakao.token-uri}")
+    private String tokenUri;
+
+    @Value("${kakao.user-info-uri}")
+    private String userInfoUri;
+
+    public String getAccessToken(String code) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", clientId);
+        params.add("redirect_uri", redirectUri);
+        params.add("code", code);
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+
+        ResponseEntity<String> response = restTemplate.postForEntity(tokenUri, request, String.class);
+
+        try {
+            JsonNode jsonNode = objectMapper.readTree(response.getBody());
+            return jsonNode.get("access_token").asText();
+        } catch (Exception e) {
+            throw new RuntimeException("카카오 access token 파싱 실패", e);
+        }
+    }
+
+    public UserInfo getKakaoUserInfo(String accessToken) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(accessToken);
+
+        HttpEntity<Void> request = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                userInfoUri,
+                HttpMethod.GET,
+                request,
+                String.class
+        );
+
+        /**
+         * TODO 나중에 심사 정보 완료되어서 이름을 가져올 수 있게 되면, 이름을 가져오는 로직 추가하여 UserInfo 에 이름도 포함하기.
+         */
+        try {
+            JsonNode json = objectMapper.readTree(response.getBody());
+            Long kakaoId = json.get("id").asLong();
+//            String nickname = json.get("properties").get("nickname").asText();
+//            String email = json.get("kakao_account").get("email").asText();
+
+            return new UserInfo(kakaoId, SocialType.KAKAO);
+        } catch (Exception e) {
+            throw new RuntimeException("카카오 사용자 정보 파싱 실패", e);
+        }
+    }
+}
