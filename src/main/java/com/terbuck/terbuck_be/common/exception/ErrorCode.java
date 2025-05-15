9c8097bb2973aab5af672ea4115df8aa44dfa385
Han <hkjbrian@naver.com>
2025-05-15 17:46:01 +0900

    feat(auth) : AppleOauthService 애플 로그인 로직 구현

--- src/main/java/com/terbuck/terbuck_be/common/exception/ErrorCode.java
+++ src/main/java/com/terbuck/terbuck_be/common/exception/ErrorCode.java
@@ -11,6 +11,8 @@ public enum ErrorCode {
     AUTH_KAKAO_USERINFO_PARSING_FAIL(HttpStatus.BAD_REQUEST.value(), "카카오 유저 정보 파싱에 실패했습니다."),
     AUTH_REFRESH_TOKEN_INVALID(HttpStatus.BAD_REQUEST.value(), "인증되지 않은 리프레시 토큰입니다."),
 
+    AUTH_APPLE_AUTHORIZATION_CODE_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "애플 인증 코드를 통해서 토큰을 발급받지 못했습니다."),
+
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "해당 사용자가 존재하지 않습니다."),
     MEMBER_STUDENTID_NOT_REGISTERED(HttpStatus.BAD_REQUEST.value(), "학생증이 등록되지 않았습니다."),
 
---
+++ src/main/java/com/terbuck/terbuck_be/domain/auth/service/AppleOAuthService.java
@@ -0,0 +1,72 @@
+package com.terbuck.terbuck_be.domain.auth.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.terbuck.terbuck_be.common.enums.SocialType;
+import com.terbuck.terbuck_be.common.exception.BusinessException;
+import com.terbuck.terbuck_be.common.exception.ErrorCode;
+import com.terbuck.terbuck_be.domain.auth.dto.UserInfo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AppleOAuthService {
+
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
+
+    @Value("${apple.client-id}")
+    private String clientId;
+
+    @Value("${apple.redirect-uri}")
+    private String redirectUri;
+
+    @Value("${apple.client-secret")
+    private String clientSecret;
+
+    @Value("${apple.token-uri}")
+    private String tokenUri;
+
+    public UserInfo getAppleUserInfo(String authorizationCode, String name) {
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("grant_type", "authorization_code");
+        body.add("code", authorizationCode);
+        body.add("client_id", clientId);
+        body.add("client_secret", clientSecret);
+        body.add("redirect_uri", redirectUri);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(body, headers);
+
+        try {
+            ResponseEntity<String> response = restTemplate.exchange(
+                    tokenUri,
+                    HttpMethod.POST,
+                    request,
+                    String.class
+            );
+
+            JsonNode jsonNode = objectMapper.readTree(response.getBody());
+            String idToken = jsonNode.get("id_token").asText();
+
+            DecodedJWT decodedJWT = JWT.decode(idToken);
+            String sub = decodedJWT.getSubject();
+
+            return new UserInfo(sub, name, SocialType.APPLE);
+        } catch (Exception e) {
+            throw new BusinessException(ErrorCode.AUTH_APPLE_AUTHORIZATION_CODE_NOT_FOUND);
+        }
+    }
+}
