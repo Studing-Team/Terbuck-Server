feaa3527c5abb372deeab1a4a2cb6bd858830d3b
Han <hkjbrian@naver.com>
2025-06-04 18:42:46 +0900

    feat(shop) : Home 화면 업체 조회 API에서 먹고가기, 이용하기로 분류하여 카테고리 처리

--- src/main/java/com/terbuck/terbuck_be/domain/shop/controller/ShopController.java
+++ src/main/java/com/terbuck/terbuck_be/domain/shop/controller/ShopController.java
@@ -7,6 +7,7 @@ import com.terbuck.terbuck_be.domain.shop.dto.HomeShopDto;
 import com.terbuck.terbuck_be.domain.shop.dto.MapShopDto;
 import com.terbuck.terbuck_be.domain.shop.dto.ShopListResponse;
 import com.terbuck.terbuck_be.domain.shop.dto.ShopResponse;
+import com.terbuck.terbuck_be.domain.shop.entity.HomeCategory;
 import com.terbuck.terbuck_be.domain.shop.entity.Location;
 import com.terbuck.terbuck_be.domain.shop.entity.ShopCategory;
 import com.terbuck.terbuck_be.domain.shop.service.ShopService;
@@ -16,6 +17,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -29,7 +31,7 @@ public class ShopController {
     @GetMapping("/home")
     public ResponseEntity<SuccessStatusResponse<ShopListResponse<HomeShopDto>>> getHomeShop(
             @RequestParam University university,
-            @RequestParam(name = "category", required = false) List<ShopCategory> categoryList,
+            @RequestParam(name = "category") HomeCategory homeCategory,
             @RequestParam(required = false) Double latitude,
             @RequestParam(required = false) Double longitude) {
 
@@ -37,6 +39,21 @@ public class ShopController {
         if (latitude != null && longitude != null) {
             location = new Location(latitude, longitude);
         }
+
+        List<ShopCategory> categoryList = new ArrayList<>();
+        if (homeCategory == HomeCategory.먹고가기) {
+            categoryList.add(ShopCategory.음식점);
+            categoryList.add(ShopCategory.카페);
+            categoryList.add(ShopCategory.주점);
+        }
+
+        if ( homeCategory == HomeCategory.이용하기) {
+            categoryList.add(ShopCategory.운동);
+            categoryList.add(ShopCategory.문화);
+            categoryList.add(ShopCategory.병원);
+            categoryList.add(ShopCategory.스터디);
+        }
+
         ShopListResponse<HomeShopDto> homeShopListResponse = shopService.getHomeShop(university, categoryList, location);
 
         return ResponseEntity
---
+++ src/main/java/com/terbuck/terbuck_be/domain/shop/entity/HomeCategory.java
@@ -0,0 +1,7 @@
+package com.terbuck.terbuck_be.domain.shop.entity;
+
+public enum HomeCategory {
+
+    먹고가기,
+    이용하기
+}
