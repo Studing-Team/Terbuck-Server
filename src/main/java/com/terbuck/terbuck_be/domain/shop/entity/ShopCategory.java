079b87791e7d839037babb99f58eef7d49e44f21
Han <hkjbrian@naver.com>
2025-06-04 18:41:22 +0900

    feat(shop) : ShopCategory 병원, 스터디 추가

--- src/main/java/com/terbuck/terbuck_be/domain/shop/entity/ShopCategory.java
+++ src/main/java/com/terbuck/terbuck_be/domain/shop/entity/ShopCategory.java
@@ -2,9 +2,14 @@ package com.terbuck.terbuck_be.domain.shop.entity;
 
 public enum ShopCategory {
 
+    //먹고가기
     음식점,
     카페,
-    문화,
     주점,
-    운동
+
+    //이용하기
+    문화,
+    운동,
+    병원,
+    스터디
 }
