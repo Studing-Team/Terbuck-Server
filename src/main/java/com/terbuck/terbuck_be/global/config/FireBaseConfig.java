9bbcd94690a9775b95177cd4072b963a0c6a7e04
Han <hkjbrian@naver.com>
2025-06-19 13:11:02 +0900

    feat(firebase) : firebase 설정 파일

---
+++ src/main/java/com/terbuck/terbuck_be/global/config/FireBaseConfig.java
@@ -0,0 +1,30 @@
+package com.terbuck.terbuck_be.global.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.InputStream;
+
+@Configuration
+public class FireBaseConfig {
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            InputStream serviceAccount = getClass().getResourceAsStream("/firebase/firebase-key.json");
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
