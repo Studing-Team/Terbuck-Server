2b3e1fa042d358c2f1f89e346dd0d3b972dc1723
Han <hkjbrian@naver.com>
2025-06-26 16:20:40 +0900

    feat(member) : 로그인 성공 응답으로 id 포함

--- src/main/java/com/terbuck/terbuck_be/domain/auth/dto/LoginResponse.java
+++ src/main/java/com/terbuck/terbuck_be/domain/auth/dto/LoginResponse.java
@@ -5,14 +5,16 @@ import lombok.Data;
 @Data
 public class LoginResponse {
     private Boolean redirect;
+    private Long id;
     private String accessToken;
     private String refreshToken;
 
     public static LoginResponse of(LoginResult loginResult) {
         LoginResponse loginResponse = new LoginResponse();
+        loginResponse.setRedirect(loginResult.needsMoreInfo());
+        loginResponse.setId(loginResult.MemberId());
         loginResponse.setAccessToken(loginResult.accessToken());
         loginResponse.setRefreshToken(loginResult.refreshToken());
-        loginResponse.setRedirect(loginResult.needsMoreInfo());
 
         return loginResponse;
     }
