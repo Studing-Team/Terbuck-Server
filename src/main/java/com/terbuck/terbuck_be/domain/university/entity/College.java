eabbea83b885b6869786f2baf7b72c44cae59145
Han <hkjbrian@gmail.com>
2025-09-18 21:27:52 +0900

    feat: 단과대학 엔티티, 레포지토리 생성

---
+++ src/main/java/com/terbuck/terbuck_be/domain/university/entity/College.java
@@ -0,0 +1,38 @@
+package com.terbuck.terbuck_be.domain.university.entity;
+
+import com.terbuck.terbuck_be.common.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class College extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "university_id")
+    private University university;
+
+    @Builder
+    public College(String name, University university) {
+        this.name = name;
+        this.university = university;
+    }
+}
---
+++ src/main/java/com/terbuck/terbuck_be/domain/university/repository/CollegeRepository.java
@@ -0,0 +1,10 @@
+package com.terbuck.terbuck_be.domain.university.repository;
+
+import com.terbuck.terbuck_be.domain.university.entity.College;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CollegeRepository extends JpaRepository<College, Long> {
+    List<College> findByUniversityId(Long universityId);
+}
